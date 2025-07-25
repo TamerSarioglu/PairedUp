8d68636dc33a9771d9fcd38cec04193d0fa992fe
Tamer Sarioglu <tamerthedark@hotmail.com>
2025-07-25 15:08:44 +0300

    refactor: remove commented-out code and improve readability in various files

--- app/src/main/java/com/tamersarioglu/pairedup/presentation/screens/home/HomeUiEvent.kt
+++
@@ -1,9 +0,0 @@
-package com.tamersarioglu.pairedup.presentation.screens.home
-
-sealed class HomeUiEvent {
-    data object NavigateToGameSetup : HomeUiEvent()
-    data object NavigateToScores : HomeUiEvent()
-    data object NavigateToSettings : HomeUiEvent()
-    data object Refresh : HomeUiEvent()
-    data object ClearError : HomeUiEvent()
-}
